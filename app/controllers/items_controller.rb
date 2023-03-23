ba6b2585efe88eb4c857281865b31ddcf13ffa3e
Alex <alexmwangithuku001@gmail.com>
2023-03-23 13:27:17 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.includes(:user).all # Eager loading user to avoid N+1 queries
+    render json: @items.to_json(include: { user: { only: [:id, :username, :city] } })
+  end
 end
